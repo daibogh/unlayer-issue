5df73de7aa5ffe2a9a848f2452473d29f77352b4
daibogh <daibogh@gmail.com>
2021-11-30 20:31:55 +0300

    added unlayer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useRef} from "react";
+import EmailEditor from 'react-email-editor';
 function App() {
+  const emailEditorRef = useRef(null);
+
+  const exportHtml = () => {
+    emailEditorRef.current.editor.exportHtml((data) => {
+      const { design, html } = data;
+      console.log('exportHtml', html);
+    });
+  };
+
+  const onLoad = () => {
+    // editor instance is created
+    // you can load your template here;
+    // const templateJson = {};
+    // emailEditorRef.current.editor.loadDesign(templateJson);
+  }
+
+  const onReady = () => {
+    // editor is ready
+    console.log('onReady');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div>
+          <button onClick={exportHtml}>Export HTML</button>
+        </div>
+
+        <EmailEditor ref={emailEditorRef} onLoad={onLoad} onReady={onReady} />
+      </div>
   );
 }
 
